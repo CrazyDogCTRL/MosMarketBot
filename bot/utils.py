e24c67c15eb182ae8b32976d96b34d9522d3322e
CrazyDogCTRL <CrazyDogCTRL@yandex.ru>
2024-06-25 10:45:47 +0800

    добавлена кнопка назад

--- bot/utils.py
+++ bot/utils.py
@@ -6,55 +6,63 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 from dotenv import load_dotenv
 from database.portfolio_db import db_path
 import os
-
+from start import bot
 load_dotenv()
-moscow_exchange_url = "https://iss.moex.com/iss/engines/stock/markets/shares/securities/{}.json"  # URL для получения данных с Московской биржи
-
+moscow_exchange_url = "https://iss.moex.com/iss/engines/stock/markets/shares/securities/{}.json"
 
 def get_portfolio(user_id):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute("SELECT name, type, purchase_date, purchase_price, ticker FROM portfolio WHERE id_user = ?",
-                   (user_id,))
+    cursor.execute("SELECT name, type, purchase_date, purchase_price, ticker FROM portfolio WHERE id_user = ?", (user_id,))
     portfolio = cursor.fetchall()
     conn.close()
     return portfolio
 
-
 def get_current_price(ticker):
+    url = f'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{ticker}.json'
     try:
-        response = requests.get(moscow_exchange_url.format(ticker))
+        response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        if 'marketdata' in data['securities']:
-            current_price = data['securities']['marketdata'][0]['LAST']
-            return current_price
-    except (requests.exceptions.RequestException, KeyError, IndexError) as e:
-        print(f"Error fetching price for {ticker}: {e}")
-    return None
+        marketdata = data.get('marketdata')
 
+        if not marketdata:
+            return None
+
+        columns = marketdata.get('columns', [])
+        if 'LAST' not in columns:
+            return None
+
+        last_index = columns.index('LAST')
+        market_data = marketdata.get('data', [])
+        if not market_data:
+            return None
+
+        current_price = market_data[0][last_index]
+        return current_price
+    except (requests.RequestException, ValueError, KeyError):
+        return None
 
 def create_excel(user_id, save_path='.'):
     portfolio = get_portfolio(user_id)
     if not portfolio:
         print("Портфель пуст или не существует.")
-        return
+        return None
 
     data = []
     for item in portfolio:
         name, type_, purchase_date, purchase_price, ticker = item
         current_price = get_current_price(ticker)
         if current_price is not None:
-            change = ((current_price - purchase_price) / purchase_price) * 100
+            change = ((current_price - purchase_price) / purchase_price) * 100 if purchase_price != 0 else None
         else:
-            current_price = "None"
-            change = "None"
+            current_price = None
+            change = None
 
         link = f"https://www.moex.com/ru/issue.aspx?code={ticker}"
         data.append([name, type_, purchase_date, purchase_price, current_price, change, ticker, link])
 
-    columns = ["Имя", "Тип", "Дата покупки", "Цена покупки", "Текущая цена", "Изменение (%)", "Тикер",
-               "Ссылка на Московскую биржу"]
+    columns = ["Имя", "Тип", "Дата покупки", "Цена покупки", "Текущая цена", "Изменение (%)", "Тикер", "Ссылка на Московскую биржу"]
     df = pd.DataFrame(data, columns=columns)
 
     wb = Workbook()
@@ -68,9 +76,25 @@ def create_excel(user_id, save_path='.'):
     full_path = os.path.join(save_path, filename)
     wb.save(full_path)
     print(f"Excel файл '{full_path}' успешно создан.")
+    return full_path
+
+
+def send_portfolio_as_excel(message):
+    try:
+        save_directory = '/Users/crazydogctrl/PycharmProjects/TGbotMosBirga'
+        file_path = create_excel(message.chat.id, save_path=save_directory)
+        if file_path is None:
+            bot.send_message(message.chat.id, "Произошла ошибка при создании Excel файла.")
+            return
+        with open(file_path, 'rb') as file:
+            bot.send_document(message.chat.id, file)
+        os.remove(file_path)  # Удаление файла после отправки
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Произошла ошибка при создании Excel файла: {e}")
 
 
 # Пример вызова функции
-user_id = 730171729  # Замените на ID пользователя
+user_id = 730171729
+print("hello")
 save_directory = '/Users/crazydogctrl/PycharmProjects/TGbotMosBirga'
 create_excel(user_id, save_path=save_directory)
--- start.py
+++ start.py
@@ -5,6 +5,7 @@ from telebot import types
 from database.portfolio_db import create_portfolio, delete_portfolio, check_portfolio_exists
 from bot.stocks import *
 from bot.bonds import *
+from bot.utils import *
 
 load_dotenv()
 
@@ -12,12 +13,14 @@ load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=["start"])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Создать портфель")
     markup.add(item1)
     bot.send_message(message.chat.id, 'Добро пожаловать в наш телеграмм бот!', reply_markup=markup)
+
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
@@ -34,8 +37,8 @@ def handle_text(message):
     elif message.text == "Управление портфелем":
         show_portfolio_management_menu(message)
     elif message.text == "Получить портфель в виде таблицы":
-        print('give_table(message)')
-    elif message.text == "Получить граф":
+        send_portfolio_as_excel(message)
+    elif message.text == "Получить график":
         print('give_graph(message)')
     elif message.text == "Добавить акцию":
         if check_portfolio_exists(message.chat.id):
@@ -61,17 +64,24 @@ def handle_text(message):
             bot.register_next_step_handler(message, delete_bond)
         else:
             bot.send_message(message.chat.id, "У вас нет портфеля для удаления облигаций.")
+    elif message.text == "Назад к главному меню":
+        show_main_menu(message)
+    elif message.text == "Назад к управлению портфелем":
+        show_portfolio_management_menu(message)
+
 
 def show_main_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Посмотреть портфель")
     item2 = types.KeyboardButton("Управление портфелем")
     item3 = types.KeyboardButton("Получить портфель в виде таблицы")
-    item4 = types.KeyboardButton("Получить граф")
+    item4 = types.KeyboardButton("Получить график")
+    item_back = types.KeyboardButton("Назад к главному меню")
     markup.row(item1)
     markup.row(item2)
     markup.row(item3, item4)
     bot.send_message(message.chat.id, 'Выберите действие:', reply_markup=markup)
+
 
 def show_portfolio_management_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -79,9 +89,12 @@ def show_portfolio_management_menu(message):
     item2 = types.KeyboardButton("Удалить акцию")
     item3 = types.KeyboardButton("Добавить облигацию")
     item4 = types.KeyboardButton("Удалить облигацию")
+    item_back = types.KeyboardButton("Назад к главному меню")
     markup.row(item1, item2)
     markup.row(item3, item4)
+    markup.row(item_back)
     bot.send_message(message.chat.id, 'Управление портфелем:', reply_markup=markup)
+
 
 # Запуск бота
 if __name__ == "__main__":
